858a8b0b6db1feabd6a37228d0b108665ca4e1b8
mka-Mikhail <mixailrojnow@yandex.ru>
2022-02-04 14:51:02 +0300

    Решение к ДЗ6*
    Создан один массив вместо двух для всех животных, нет вызова методов животных
    Вторая версия программы про животных

--- src/lesson_6/Main.java
+++ src/lesson_6/Main.java
@@ -8,12 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int amountDog = 4;  //Кол-во собак, которых мы можем создать
-        int amountCat = 5;  //Кол-во котов, которых мы можем создать
+        int amountAnimals = 10;
+        //int amountDog = 4;  //Кол-во собак, которых мы можем создать
+        //int amountCat = 5;  //Кол-во котов, которых мы можем создать
 
-        Dog[] arrayDog = new Dog[amountDog];
-        Cat[] arrayCat = new Cat[amountCat];
+        Animals[] animals = new Animals[amountAnimals];
 
+        //Dog[] arrayDog = new Dog[amountDog];
+        //Cat[] arrayCat = new Cat[amountCat];
+
+        int countAnimals = 0;  //Счётчик всех животных
         int countDog = 0;  //Счётчик собак
         int countCat = 0;  //Счётчик котов
 
@@ -28,10 +32,12 @@ public class Main {
 
             switch (animal) {
                 case 1:
-                    countDog = seatCheck(arrayDog, countDog, amountDog, 1, "СОБАКИ");  //Создание собак
+                    countAnimals = seatCheck(animals, countAnimals, amountAnimals, 1, "СОБАКИ");  //Создание собак
+                    countDog++;
                     break;
                 case 2:
-                    countCat = seatCheck(arrayCat, countCat, amountCat, 2, "КОТА (КОШКИ)");  //Создание котов
+                    countAnimals = seatCheck(animals, countAnimals, amountAnimals, 2, "КОТА (КОШКИ)");  //Создание котов
+                    countCat++;
                     break;
             }
 
@@ -44,9 +50,9 @@ public class Main {
                 break;
         }
 
-        getInfoOfAnimals(countDog, countCat);
+        getInfoOfAnimals(countDog, countCat, countAnimals);
 
-        for (int i = 0; i < countDog; i++) {
+        /*for (int i = 0; i < countDog; i++) {
             System.out.println();
             arrayDog[i].run(250);
             arrayDog[i].run(501);
@@ -58,25 +64,25 @@ public class Main {
             arrayCat[i].run(167);
             arrayCat[i].run(257);
             arrayCat[i].swim(23);
-        }
+        }*/
     }
 
     //Вывод информации о животных
-    public static void getInfoOfAnimals(int countDog, int countCat) {
+    public static void getInfoOfAnimals(int countDog, int countCat, int countAnimals) {
 
         System.out.println("\n*****************");
         System.out.println("Кол-во созданных животных:" +
                 "\nСобак - " + countDog +
                 "\nКотов и кошек - " + countCat +
-                "\nВсего животных - " + (countDog+countCat));
+                "\nВсего животных - " + countAnimals);
         System.out.println("*****************");
     }
 
     //Проверка на наличие мест и вызов метода создания животного
-    public static int seatCheck(Object[] array, int count, int amount, int numOfAnimal, String animal) {
+    public static int seatCheck(Object[] animals, int count, int amount, int numOfAnimal, String animal) {
 
         if (count < amount) {
-            arrayFilling(array, count, numOfAnimal, animal);
+            arrayFilling(animals, count, numOfAnimal, animal);
             count++;
         }
         else {
@@ -86,17 +92,17 @@ public class Main {
     }
 
     //Создание животного, животное определяется по номеру
-    public static void arrayFilling(Object[] array, int i, int numOfAnimal, String animal) {
+    public static void arrayFilling(Object[] animals, int i, int numOfAnimal, String animal) {
 
         System.out.print("\nВведите имя " + animal +": ");
         String animalName = scanner.next();
 
         switch (numOfAnimal) {
             case 1:
-                array[i] = new Dog(animalName);
+                animals[i] = new Dog(animalName);
                 break;
             case 2:
-                array[i] = new Cat(animalName);
+                animals[i] = new Cat(animalName);
                 break;
         }
     }
