c22838a1b722dc5bfdb4d38c0297dfdf3e65b381
mka-Mikhail <mixailrojnow@yandex.ru>
2022-02-05 21:32:15 +0300

    Класс Cat

--- src/lesson_7/Cat.java
+++ src/lesson_7/Cat.java
@@ -1,4 +1,34 @@
 package lesson_7;
 
 public class Cat {
+
+    private String name;
+    private boolean satiety;
+    private int appetite;
+
+    public Cat(String name, boolean satiety, int appetite) {
+        this.name = name;
+        this.satiety = satiety;
+        this.appetite = appetite;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(boolean satiety) {
+        this.satiety = satiety;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", satiety=" + satiety +
+                '}';
+    }
 }
