d9cf7ba006b684a1b760ae6a8d9ee741225149bc
mka-Mikhail <mixailrojnow@yandex.ru>
2022-02-05 21:18:47 +0300

    Решение к ДЗ7
    Новый функционал программы

--- src/lesson_7/Main.java
+++ src/lesson_7/Main.java
@@ -1,32 +1,65 @@
 package lesson_7;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         Cat[] cats = new Cat[3];
 
-        Plate plate = new Plate(20);
+        Plate plate = new Plate(5);
 
-        cats[0] = new Cat("Барсик", false, 10);
-        cats[1] = new Cat("Кузя", false, 15);
-        cats[2] = new Cat("Мурзик", false, 18);
+        cats[0] = new Cat("Барсик", false,  10);
+        cats[1] = new Cat("Кузя",   false,  15);
+        cats[2] = new Cat("Мурзик", false,  18);
 
-        System.out.println(plate.getFood());
         getInfoOfCats(cats);
 
-        for (int i = 0; i < 3; i++) {
-            if (plate.getFood() - cats[i].getAppetite() > 0) {
-                plate.setFood(plate.getFood() - cats[i].getAppetite());
-                cats[i].setSatiety(true);
+        int countCats = 0;
+
+        while (true) {
+
+            if (plate.getFood() - cats[countCats].getAppetite() >= 0) {
+
+                plate.setFood(plate.getFood() - cats[countCats].getAppetite());
+                cats[countCats].setSatiety(true);
+                System.out.println("\n" + cats[countCats].getName() + " поел");
+                countCats++;
             }
+            else {
+
+                System.out.println("\nЕды не хватает\n" +
+                        "Кол-во еды в тарелке " + plate.getFood());
+                System.out.println("Кормить " + cats[countCats].getName() + "? (1 - да, 0 - нет)");
+                int choose = scanner.nextInt();
+
+                switch (choose) {
+
+                    case 1:
+                        System.out.println("Сколько добавить еды в тарелку?");
+                        int addFood = scanner.nextInt();
+                        plate.addFood(addFood);
+                        System.out.println("В тарелку добавили " + addFood + " еды");
+                        break;
+
+                    case 0:
+                        countCats++;  //Переходим к следуюущему коту
+                        break;
+                }
+            }
+
+            if (countCats == 3) break;
         }
 
-        System.out.println(plate.getFood());
         getInfoOfCats(cats);
     }
 
-    private static void getInfoOfCats(Cat[] cats) {
+    public static void getInfoOfCats(Cat[] cats) {
+
+        System.out.println();
         for (Cat c :
                 cats) {
             System.out.println(c);
@@ -34,3 +67,4 @@ public class Main {
         System.out.println();
     }
 }
+
