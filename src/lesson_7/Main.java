8bfa580c5e07ab46ce344cecf5dca9db80497106
mka-Mikhail <mixailrojnow@yandex.ru>
2022-02-05 21:34:07 +0300

    Класс Main

--- src/lesson_7/Main.java
+++ src/lesson_7/Main.java
@@ -1,4 +1,36 @@
 package lesson_7;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Cat[] cats = new Cat[3];
+
+        Plate plate = new Plate(20);
+
+        cats[0] = new Cat("Барсик", false, 10);
+        cats[1] = new Cat("Кузя", false, 15);
+        cats[2] = new Cat("Мурзик", false, 18);
+
+        System.out.println(plate.getFood());
+        getInfoOfCats(cats);
+
+        for (int i = 0; i < 3; i++) {
+            if (plate.getFood() - cats[i].getAppetite() > 0) {
+                plate.setFood(plate.getFood() - cats[i].getAppetite());
+                cats[i].setSatiety(true);
+            }
+        }
+
+        System.out.println(plate.getFood());
+        getInfoOfCats(cats);
+    }
+
+    private static void getInfoOfCats(Cat[] cats) {
+        for (Cat c :
+                cats) {
+            System.out.println(c);
+        }
+        System.out.println();
+    }
 }
